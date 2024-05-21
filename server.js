b4f26dbf7df500c4e4fe869118b3bad4ec4fe21e
Laura Long <lauralonggone@gmail.com>
2024-05-20 20:46:21 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import knex from 'knex'
+import knexConfig from './knexfile.js'
+import { Model } from 'objection'
+
+const app = express()
+const db = knex(knexConfig.development)
+
+app.set('port', process.env.PORT || 3001)
+app.locals.title = 'Kane Creek Comments'
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+  response.send('Welcome to Kane Creek Comments!')
+})
+
+app.get(responses, async (req, res) => {
+    try { 
+        const responses = await db.select('*').from('responses')
+        res.status(200).json(responses)
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get responses" })
+    }
+})
+
+app.listen(app.get('port'), () => {
+  console.log(
+    `${app.locals.title} is running on http://localhost:${app.get('port')}.`
+  )
+})
