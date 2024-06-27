a99023bd2e4e9bf2113793430bbae8f4bf4fdcc9
Laura Long <lauralonggone@gmail.com>
2024-06-27 15:05:53 -0600

    Add morgan logging

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 import express from 'express'
 import knex from 'knex'
 import knexConfig from './knexfile.js'
+import morgan from 'morgan'
 
 const app = express()
 const db = knex(knexConfig.development)
+// var morgan = require('morgan')
 
 app.set('port', process.env.PORT || 3001)
 app.locals.title = 'Kane Creek Comments'
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 app.get('/', (request, response) => {
   response.send('Welcome to Kane Creek Comments!')
