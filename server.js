7816fda3bf5ae9321dae329284138e9a59593aa5
Laura Long <lauralonggone@gmail.com>
2024-06-27 19:56:19 -0600

    Add logging to the server

--- server.js
+++ server.js
@@ -1,34 +1,33 @@
-import express from 'express'
-import knex from 'knex'
-import knexConfig from './knexfile.js'
-import morgan from 'morgan'
+import express from 'express';
+import knex from 'knex';
+import knexConfig from './knexfile.js';
+import morgan from 'morgan';
 
-const app = express()
-const db = knex(knexConfig.development)
+const app = express();
+const db = knex(knexConfig.production);
 
-app.set('port', process.env.PORT || 3001)
-app.locals.title = 'Kane Creek Comments'
+console.log('DB Configuration:', knexConfig.production);
 
-app.use(express.json())
+app.set('port', process.env.PORT || 3001);
+app.locals.title = 'Kane Creek Comments';
 
-app.use(morgan('tiny'))
+app.use(morgan('dev'));
+app.use(express.json());
 
 app.get('/', (request, response) => {
-  response.send('Welcome to Kane Creek Comments!')
-})
+  response.send('Welcome to Kane Creek Comments!');
+});
 
 app.get('/responses', async (req, res) => {
-    try { 
-        const responses = await db.select('*').from('responses') //.limit(1000)
-        res.status(200).json(responses)
-    } catch (error) {
-      console.error('Error:', error)
-        res.status(500).json({ error: "Failed to get responses" })
-    }
-})
+  try { 
+    const responses = await db.select('*').from('responses').whereNotNull('comment');
+    res.status(200).json(responses);
+  } catch (error) {
+    console.error('Error fetching responses:', error);
+    res.status(500).json({ error: "Failed to get responses" });
+  }
+});
 
 app.listen(app.get('port'), () => {
-  console.log(
-    `${app.locals.title} is running on http://localhost:${app.get('port')}.`
-  )
-})
+  console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+});
