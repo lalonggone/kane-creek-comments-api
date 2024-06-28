482f08aa396ee1668232474451446c6405500e2d
Laura Long <lauralonggone@gmail.com>
2024-06-27 20:06:10 -0600

    Update Knex configuration with SSL

--- knexfile.js
+++ knexfile.js
@@ -20,7 +20,12 @@ const knexConfig = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       min: 2,
       max: 15,
